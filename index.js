49fe9b6e73e70ba5e8fe0587bacf3811cfbd1127
Abdul3147 <abdallahussein228@gmail.com>
2024-07-11 22:35:05 +0300

    completed

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet (feet){
+  if(feet > 2500){
+    return 'No can do.';
+  }
+  else if(feet > 2000) {
+    return 'I will gladly take your thirty bucks.';
+  }
+  else if(feet < 2000) {
+    return 'That will be twenty bucks.';
+  }
+else {
+  return 'This one is on me!';
+}
   // Write your code here!
 }
+console.log(scuberGreetingForFeet);
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
+  return city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
   // Write your code here!
 }
+console.log(ternaryCheckCity);
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
+  switch (tip) {
+    case 'generous':
+      return 'Thank you so much.';
+    case 'not as generous':
+      return 'Thank you.';
+    default:
+      return 'Bye.';
+  }
   // Write your code here!
 }
+console.log(switchOnCharmFromTip);
